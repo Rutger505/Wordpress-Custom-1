0df3f532220258cb9cae55f9104e559bcde52b57
Rutger Pronk <rutger.pronk11@gmail.com>
2025-10-08 14:09:47 +0200

    Remove static posts from sidebar.php

--- wp-content/themes/greentech-solutions/sidebar.php
+++ wp-content/themes/greentech-solutions/sidebar.php
@@ -46,63 +46,6 @@
 			endif;
 			?>
 
-            <!-- Static Mini Posts Below -->
-            <!-- Mini Post -->
-            <article class="mini-post">
-                <header>
-                    <h3><a href="single.html">Vitae sed condimentum</a></h3>
-                    <time class="published" datetime="2015-10-20">October 20, 2015</time>
-                    <a href="#" class="author"><img
-                                src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/avatar.jpg"
-                                alt=""/></a>
-                </header>
-                <a href="single.html" class="image"><img
-                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/pic04.jpg"
-                            alt=""/></a>
-            </article>
-
-            <!-- Mini Post -->
-            <article class="mini-post">
-                <header>
-                    <h3><a href="single.html">Rutrum neque accumsan</a></h3>
-                    <time class="published" datetime="2015-10-19">October 19, 2015</time>
-                    <a href="#" class="author"><img
-                                src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/avatar.jpg"
-                                alt=""/></a>
-                </header>
-                <a href="single.html" class="image"><img
-                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/pic05.jpg"
-                            alt=""/></a>
-            </article>
-
-            <!-- Mini Post -->
-            <article class="mini-post">
-                <header>
-                    <h3><a href="single.html">Odio congue mattis</a></h3>
-                    <time class="published" datetime="2015-10-18">October 18, 2015</time>
-                    <a href="#" class="author"><img
-                                src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/avatar.jpg"
-                                alt=""/></a>
-                </header>
-                <a href="single.html" class="image"><img
-                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/pic06.jpg"
-                            alt=""/></a>
-            </article>
-
-            <!-- Mini Post -->
-            <article class="mini-post">
-                <header>
-                    <h3><a href="single.html">Enim nisl veroeros</a></h3>
-                    <time class="published" datetime="2015-10-17">October 17, 2015</time>
-                    <a href="#" class="author"><img
-                                src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/avatar.jpg"
-                                alt=""/></a>
-                </header>
-                <a href="single.html" class="image"><img
-                            src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/pic07.jpg"
-                            alt=""/></a>
-            </article>
-
         </div>
     </section>
 
