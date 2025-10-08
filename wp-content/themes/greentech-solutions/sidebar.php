ef38fdcef2742ead74d06803e466c2bc0bf21195
Rutger Pronk <rutger.pronk11@gmail.com>
2025-10-08 13:59:50 +0200

    Remove static content from homepage sidebar and single.php

--- wp-content/themes/greentech-solutions/sidebar.php
+++ wp-content/themes/greentech-solutions/sidebar.php
@@ -138,63 +138,6 @@
 				wp_reset_postdata();
 			endif;
 			?>
-
-            <!-- Static Posts Below -->
-            <li>
-                <article>
-                    <header>
-                        <h3><a href="single.html">Lorem ipsum fermentum ut nisl vitae</a></h3>
-                        <time class="published" datetime="2015-10-20">October 20, 2015</time>
-                    </header>
-                    <a href="single.html" class="image"><img
-                                src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/pic08.jpg"
-                                alt=""/></a>
-                </article>
-            </li>
-            <li>
-                <article>
-                    <header>
-                        <h3><a href="single.html">Convallis maximus nisl mattis nunc id lorem</a></h3>
-                        <time class="published" datetime="2015-10-15">October 15, 2015</time>
-                    </header>
-                    <a href="single.html" class="image"><img
-                                src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/pic09.jpg"
-                                alt=""/></a>
-                </article>
-            </li>
-            <li>
-                <article>
-                    <header>
-                        <h3><a href="single.html">Euismod amet placerat vivamus porttitor</a></h3>
-                        <time class="published" datetime="2015-10-10">October 10, 2015</time>
-                    </header>
-                    <a href="single.html" class="image"><img
-                                src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/pic10.jpg"
-                                alt=""/></a>
-                </article>
-            </li>
-            <li>
-                <article>
-                    <header>
-                        <h3><a href="single.html">Magna enim accumsan tortor cursus ultricies</a></h3>
-                        <time class="published" datetime="2015-10-08">October 8, 2015</time>
-                    </header>
-                    <a href="single.html" class="image"><img
-                                src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/pic11.jpg"
-                                alt=""/></a>
-                </article>
-            </li>
-            <li>
-                <article>
-                    <header>
-                        <h3><a href="single.html">Congue ullam corper lorem ipsum dolor</a></h3>
-                        <time class="published" datetime="2015-10-06">October 7, 2015</time>
-                    </header>
-                    <a href="single.html" class="image"><img
-                                src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/pic12.jpg"
-                                alt=""/></a>
-                </article>
-            </li>
         </ul>
     </section>
 
