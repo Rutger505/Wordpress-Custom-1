3616ef4815b76620ef9d6991abc6318852c21edc
Rutger Pronk <rutger.pronk11@gmail.com>
2025-10-08 11:52:49 +0200

    Start setting up theme

---
+++ wp-content/themes/greentech-solutions/header.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="logo">
+
+        <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+            <img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="logo">
+        </a></div>
+    <p><?php bloginfo( 'description' ); ?></p>
+</header>
+
