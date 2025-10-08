1fb51eb4e879fd02a607fc922d1a25f190fd1660
Rutger Pronk <rutger.pronk11@gmail.com>
2025-10-08 12:20:54 +0200

    Make the template website be the base of the wordpress theme

--- wp-content/themes/greentech-solutions/header.php
+++ wp-content/themes/greentech-solutions/header.php
@@ -1,9 +1,17 @@
-<header>
+<header class="flex gap-5 divide-solid">
     <div class="logo">
 
         <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
             <img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="logo">
         </a></div>
-    <p><?php bloginfo( 'description' ); ?></p>
+    <nav class="flex gap-5 divide-x-3 divide-indigo-500 divide-solid">
+        <ul>
+            <li><a href="#">Lorem</a></li>
+            <li><a href="#">Ipsum</a></li>
+            <li><a href="#">Feugiat</a></li>
+            <li><a href="#">Tempus</a></li>
+            <li><a href="#">Adipiscing</a></li>
+        </ul>
+    </nav>
 </header>
 
