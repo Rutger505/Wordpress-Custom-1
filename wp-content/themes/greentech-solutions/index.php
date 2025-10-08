e4e92b43542c6d0d250f885925da633755efb16c
Rutger Pronk <rutger.pronk11@gmail.com>
2025-10-08 13:40:37 +0200

    Create a single.php from the single.html with the wordpress semantics

--- wp-content/themes/greentech-solutions/index.php
+++ wp-content/themes/greentech-solutions/index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>GreenTech Solutions</title>
-    <meta charset="utf-8"/>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+    <title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
     <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/sass/main.css"/>
 	<?php wp_head(); ?>
 </head>
